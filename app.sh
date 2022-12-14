fc1332d89b78479ff696bd3cc272936ada25157e
yeyuchen198 <89305403+yeyuchen198@users.noreply.github.com>
2022-12-14 11:18:20 +0800

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 
-install -m 755 /usr/local/app/myapp /usr/local/bin/myapp
-install -m 755 /usr/local/app/v2ctl /usr/local/bin/v2ctl
-
-
-#run myapp
-# mkdir /etc/myapp
-# cp /usr/local/app/app.json /etc/myapp/app.json
+# run myapp
 # notice: already "COPY app.json /etc/myapp/app.json" at Dockerfile
-/usr/local/bin/myapp -config=/etc/myapp/app.json &
+/usr/bin/myapp -config=/etc/myapp/app.json &
 
 #run nginx
 # CMD ["nginx", "-g", "daemon off;"]
